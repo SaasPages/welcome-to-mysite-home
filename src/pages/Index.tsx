c8ae1e2fbce2dbd2cc9f05aae05ea44b732d8c9b
SaasPages <myasirmalkani44@gmail.com>
2025-10-26 08:39:57 +0500

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,7 +4,7 @@ import HeroSection from "@/components/HeroSection";
 import FeaturedProperties from "@/components/FeaturedProperties";
 import StatsSection from "@/components/StatsSection";
 import TechStackSlider from "@/components/TechStackSlider";
-import VideoSection from "@/components/VideoSection;
+import VideoSection from "@/components/VideoSection";
 
 const Index = () => {
   return (
