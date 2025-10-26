505ce4f16121f19ef462d9fe4e70ba3288161f5d
SaasPages <myasirmalkani44@gmail.com>
2025-10-26 08:35:24 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import Video from "./pages/Video";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -17,6 +18,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/" element={<NotFound />} />
+          <Route path="/" element={<Video />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
